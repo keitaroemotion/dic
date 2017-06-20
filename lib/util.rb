fc9bb063a67f2e3e0ea5267d4b0493b36137368e
kei <kei@omise.co>
2017-06-20 19:58:34 +0900

    pattern matching test and its further implementation

--- lib/util.rb
+++ lib/util.rb
@@ -1,5 +1,25 @@
 module Lib
   class Util
+     STELLA = "*"
+ 
+     def self.desterisk(regex)
+       regex.gsub("*", "")
+     end
+
+     def self.match?(regex, target_string)
+       stella_head = regex.start_with?(STELLA)
+       stella_tail = regex.end_with?(STELLA)
+
+       if stella_head && stella_tail  
+         regex = desterisk(regex)
+       else  
+         regex = stella_head ? "#{desterisk(regex)}$" : regex
+         regex = stella_tail ? "^#{desterisk(regex)}" : regex
+       end
+       
+       !Regexp.new("#{regex}").match(target_string).nil? 
+     end
+
      def self.option?(arg)
        arg.start_with?("-")
      end
--- test/lib/util_test.rb
+++ test/lib/util_test.rb
@@ -7,6 +7,19 @@ class Lib::UtilTest < Minitest::Test
     @util = Lib::Util
   end
 
+  def test_match?
+    refute @util.match?("precur*", "aprecure")
+    assert @util.match?("precur*", "precure")
+    assert @util.match?("recur", "precure")
+    assert @util.match?("*recure", "precure")
+    refute @util.match?("*recu", "precure")
+    assert @util.match?("p[a-z][eE0]cu[rR]e", "precure")
+    assert @util.match?("*recur*", "precure")
+    refute @util.match?("*racur*", "precure")
+    assert @util.match?("p(recu|rocu)re", "precure")
+    assert @util.match?("prec[^i]re", "precure")
+  end
+ 
   def test_option?
     assert @util.option?("-a") 
     refute @util.option?("aa-") 
