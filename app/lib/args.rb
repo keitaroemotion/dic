dc80e338b36c69ab1067ab4dd86b0b73ebc9046a
kei <kei@omise.co>
2017-11-07 17:01:36 +0900

    args class newly defined

---
+++ app/lib/args.rb
@@ -0,0 +1,17 @@
+class Args
+  def initialize(args)
+    @args = args
+  end
+
+  def negations
+    @args.select{|a| /^[\^]/ =~ a }.map{|a| a[1..-1]}
+  end
+
+  def options
+    @args.select{|a| /^\-[^\^]+$/ =~ a }
+  end
+
+  def values
+    @args.select{|a| /^[^\-\^]/ =~ a }
+  end
+end
