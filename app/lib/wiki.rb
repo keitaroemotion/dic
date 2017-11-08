56ae25575821690b10cd70fe9a3311d2feafcdcd
kei <kei@omise.co>
2017-11-08 14:16:02 +0900

    refactor on Wiki module. Git related version control is now attributed to wiki

--- app/lib/wiki.rb
+++ app/lib/wiki.rb
@@ -5,11 +5,53 @@ class Wiki
   #
   def initialize(location)
     @location           = location
-    @original_articles  = get_markdowns_only(@location.raw)
-    @formatted_articles = get_markdowns_only(@location.pages)
+    @editor             = "vim"
+    @original_articles  = articles(@location.raw)
+    @formatted_articles = articles(@location.pages)
+  end
+
+  #
+  # XXX test
+  #
+  def save
+    system ([
+      "cd #{@location.root}",
+      "git add #{@location.root}",
+      "git commit -m \"page updated\"",
+      "git pull origin master",
+      "git push origin master",
+    ].join(";"))
+  end
+
+  #
+  # XXX test
+  #
+  def update
+    @wiki.original_articles.each { |file| convert_raw(file) }
+    backup
+    @wiki.formatted_articles.each { |file| convert_page(file) }
+    system ("cd #{@root}; git --no-pager diff #{@location.root}") 
+    abort if ask_input("ok?[Y/n]").downcase == "n"
+    save
+  end
+
+  #
+  # XXX test
+  #
+  def create(args)
+    system "#{@editor} #{@location.raw}/#{args.join('_')}.md"
+  end
+
+  #
+  # XXX test
+  # 
+  def edit(files, size)
+    size = 1 unless size < files.size
+    system "#{@editor} #{files[size - 1]}"
   end
 
   private
+
   #
   # Open/Closed principle ... following methods are closed for modification
   #                           while aboves are open for it.
@@ -17,9 +59,32 @@ class Wiki
 
   def articles(path)
     Dir["#{path}/*"]
+      .select { |file| File.file?(file) && /\.md$/ =~ file }
   end
 
-  def get_markdowns_only(path)
-    articles(path).select { |file| /\.md/ =~ file }
+  def convert_raw(file)
+    content = File.read(file)
+    content.gsub!(/(#{@location.pages}|#{@location.raw}|#{@location.root})\//, "")
+    overwrite(file, content)    
+  end
+
+  def convert_page(file)
+    content = File.read(file).split("\n").map { |line|
+      image_regex =~ line ? embedded_image_link(line) : line
+    }
+    overwrite(file, content)
+  end
+
+  def backup
+    system ([
+      "cd #{@location.root}",
+      "cp -r #{@location.raw}/* #{@location.pages}",
+    ].join(";"))
+  end
+
+  def overwrite(file, content)
+    f = File.open(File.join(pages, File.basename(file)), "w")
+    f.puts content
+    f.close
   end
 end
