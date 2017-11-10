9a522f4a7ce7c6f5039fa7483ec3f49f33de3031
kei <kei@omise.co>
2017-11-10 17:49:06 +0900

    wiki update

--- test/app/lib/wiki_test.rb
+++ test/app/lib/wiki_test.rb
@@ -1,4 +1,5 @@
 require "minitest/autorun"
+require "fileutils"
 require "./test/test_helper.rb"
 require "./app/lib/wiki.rb"
 require "./app/lib/args.rb"
@@ -7,32 +8,34 @@ require "./app/lib/location.rb"
 class WikiTest < Minitest::Test
   describe "" do
     before do
-      system "touch ./test/source/etc/vol/raw/moo_min_dani.md"
-      system "touch ./test/source/etc/vol/pages/moo_min_dani.md"
+      @raw_file   = "./test/source/etc/vol/raw/moo_min_dani.md"
+      @pages_file = "./test/source/etc/vol/pages/moo_min_dani.md"
+      FileUtils.touch(@raw_file)
+      FileUtils.touch(@pages_file)
       location = Location.new(root: "./test/source")
       args     = Args.new(["apple", "^banana", "fuji"])
       @wiki    = Wiki.new(location: location, args: args, editor: nil)
     end
 
     def test_create
-      assert_equal "{editing} ./test/source/etc/vol/raw/moo_min_dani.md", @wiki.create(["moo", "min", "dani"]) 
+      assert_equal "{editing} #{@raw_file}", @wiki.create(["moo", "min", "dani"]) 
     end
 
     def test_edit
-      assert_equal "{editing} ./test/source/etc/vol/raw/moo_min_dani.md", @wiki.edit(["./test/source/etc/vol/raw/moo_min_dani.md"], 1) 
+      assert_equal "{editing} #{@raw_file}", @wiki.edit([@raw_file], 1) 
     end
 
     def test_original_articles
-      assert_equal ["./test/source/etc/vol/raw/moo_min_dani.md"], @wiki.original_articles
+      assert_equal [@raw_file], @wiki.original_articles
     end
 
     def test_formatted_articles
-      assert_equal ["./test/source/etc/vol/pages/moo_min_dani.md"], @wiki.formatted_articles
+      assert_equal [@pages_file], @wiki.formatted_articles
     end
 
     after do
-      File.delete("./test/source/etc/vol/raw/moo_min_dani.md")
-      File.delete("./test/source/etc/vol/pages/moo_min_dani.md")
+      File.delete(@raw_file)
+      File.delete(@pages_file)
     end
   end  
 end
